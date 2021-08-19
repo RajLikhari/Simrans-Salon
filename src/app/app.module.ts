098255aefc60a71676b651ef533026654cc6b396
SikhMeme <rajslikhari@gmail.com>
2021-08-19 16:30:25 -0500

    Reorg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { BookingPageComponent } from './booking-page/booking-page.component';
+import { GalleryComponentComponent } from './gallery-component/gallery-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MenuComponent,
+    MainPageComponent,
+    FooterComponent,
+    BookingPageComponent,
+    GalleryComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MenuComponent]
 })
 export class AppModule { }
